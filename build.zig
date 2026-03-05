6355c0599a2490d4bfa9325789100e4e6b555fc5
Filippo <filippo.marini203@gmail.com>
2026-03-05 23:02:22 +0100

    capito sistema di build Zig

--- build.zig
+++ build.zig
@@ -1,3 +1,22 @@
 const std = @import("std");
 
-pub fn build(_: *std.Build) void {}
+pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "Raymarching demo",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .optimize = optimize,
+            .target = target,
+        }),
+    });
+
+    b.installArtifact(exe);
+
+    const run_exe = b.addRunArtifact(exe);
+
+    const run_step = b.step("run", "Run the executable");
+    run_step.dependOn(&run_exe.step);
+}
