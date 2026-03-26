2d62c430b243a95d326068cba404110ef3817343
Filippo <filippo.marini203@gmail.com>
2026-03-26 16:27:22 +0100

    fix vec

--- src/test.zig
+++ src/test.zig
@@ -8,13 +8,15 @@ test "engine console interface" {
     var buf: [1]u8 = undefined;
     state.console.init(Kind.STDERR, &buf);
     var writer = state.console.writer(Kind.STDERR);
-    try writer.print("[TEST] print succesful!", .{});
+    try writer.print("[TEST] print succesful!\n", .{});
     try writer.flush();
 }
 
 test "vectors" {
     const Vec3 = glm.Vec(3);
-    var v: Vec3 = .{.x = 12, .y = 3, .z = -1};
+    const v: Vec3 = .{.x = 12, .y = 3, .z = -1};
     try std.testing.expectEqual(v.length(), 12.409673646);
     try std.testing.expectEqual(v.sum(1), Vec3{.x = 13, .y = 4, .z = 0});
+    const v2: Vec3 = .{.x = 1.0/3.0, .y = 2, .z = 0};
+    try std.testing.expectEqual(v.mul(v2), Vec3{.x = 4, .y = 6, .z = 0});
 }
